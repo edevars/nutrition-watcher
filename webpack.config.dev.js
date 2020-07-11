d95de43eecfe6e247451459e78db8bc03eaf77aa
edevars <e.devars.dev@gmail.com>
2020-07-11 16:20:29 -0500

    :sparkles: Webpack config for development

---
+++ webpack.config.dev.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+
+
+
+module.exports = {
+    entry: path.join(__dirname, "/src/index.js"),
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        historyApiFallback: true,
+        open: true,
+        port: 9000,
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'assets/',
+                        }
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: path.join(__dirname, "/public/index.html"),
+                favicon: path.join(__dirname, "/public/favicon.png")
+            }
+        ),
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+}
