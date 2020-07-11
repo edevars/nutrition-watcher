540cd7061f707899f98ddd5f2dcf60078f36d5c8
edevars <e.devars.dev@gmail.com>
2020-07-11 17:34:53 -0500

    :sparkles: Adding general structure with react router dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import NotFound from '../pages/NotFound';
 
-export const App = () => {
-  return (
-    <div>
-      <h1>Hola mundo!</h1>
-    </div>
-  );
-};
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="*" component={NotFound} />
+    </Switch>
+  </Router>
+);
+
+export default App;
