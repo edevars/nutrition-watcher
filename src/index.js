874fb98f13674fb3a707cf5cde674e6212ca2824
edevars <e.devars.dev@gmail.com>
2020-07-11 16:07:13 -0500

    :sparkles: Creating basic production config for React App

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './components/App';
+import './styles/index.css'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, "/src/index.js"),
@@ -6,5 +7,42 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: 'bundle.[hash].js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: path.join(__dirname, "/public/index.html"),
+                favicon: path.join(__dirname, "/public/favicon.png")
+            }
+        )
+    ]
 }
