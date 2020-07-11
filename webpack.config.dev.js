1209a8aa7c2d9497d5e371eddd50793c6fb304cc
edevars <e.devars.dev@gmail.com>
2020-07-11 17:34:27 -0500

    :sparkles: Adding eslint with Airbnb config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,62 +1,65 @@
-const path = require('path')
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack')
-
-
+const webpack = require('webpack');
 
 module.exports = {
-    entry: path.join(__dirname, "/src/index.js"),
-    mode: "development",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
-        historyApiFallback: true,
-        open: true,
-        port: 9000,
-        hot: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+  entry: path.join(__dirname, '/src/index.jsx'),
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    historyApiFallback: true,
+    open: true,
+    port: 9000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/',
             },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react']
-                    }
-                }
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: 'assets/',
-                        }
-                    },
-                ],
-            },
+          },
         ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    plugins: [
-        new HtmlWebpackPlugin(
-            {
-                template: path.join(__dirname, "/public/index.html"),
-                favicon: path.join(__dirname, "/public/favicon.png")
-            }
-        ),
-        new webpack.HotModuleReplacementPlugin(),
-    ]
-}
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: path.join(__dirname, '/public/index.html'),
+        favicon: path.join(__dirname, '/public/favicon.png'),
+      },
+    ),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,53 @@
-const path = require('path')
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.join(__dirname, "/src/index.js"),
-    mode: "production",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: 'bundle.[hash].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react']
-                    }
-                }
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
+  entry: path.join(__dirname, '/src/index.jsx'),
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
         ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    plugins: [
-        new HtmlWebpackPlugin(
-            {
-                template: path.join(__dirname, "/public/index.html"),
-                favicon: path.join(__dirname, "/public/favicon.png")
-            }
-        )
-    ]
-}
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: path.join(__dirname, '/public/index.html'),
+        favicon: path.join(__dirname, '/public/favicon.png'),
+      },
+    ),
+  ],
+};
